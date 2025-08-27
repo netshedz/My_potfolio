06214a63e8adc3f3c1e05dcf83440bc28904ca08
netshedzo <67120128+netshedz@users.noreply.github.com>
2025-08-27 02:26:46 +0200

    .

--- index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("downloadCVBtn").addEventListener("click", function () {
-  const link = document.createElement("a");
-  link.href = "Dzhivhuho-Netshedzo-CV.docx";
-  link.download = "Dzhivhuho-Netshedzo-CV.docx";
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-});
